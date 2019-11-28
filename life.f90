1ea93eae0a2458119ced79a0f582ff6a10a55ab9
cchillen <caseyhillenburg@live.com>
2019-11-27 23:08:08 -0500

    Created module to store board functions

--- life.f90
+++ life.f90
@@ -1,3 +1,62 @@
+module mBoard
+    implicit none
+
+    contains
+
+    function createBoard(rows, cols)
+
+        logical, dimension(:,:), allocatable :: createBoard
+        integer, intent(in) :: rows, cols
+        integer :: r,c
+
+        allocate( createBoard(rows, cols) )
+
+        do c = 1,cols
+            do r = 1,rows
+                createBoard(r,c) = (nint(rand(0)) .eq. 0)
+            end do
+        end do
+
+    end function createBoard
+
+    function stepBoard(board)
+    ! stepBoard - Determines the next sequence in the board
+    !
+    ! Inputs:
+    !   board - 2D array of logicals that represents the state of the game.
+
+        logical, dimension(:,:), intent(in) :: board
+        logical, dimension(:,:), allocatable :: stepBoard
+
+    end function stepBoard
+
+    subroutine printBoard(board)
+    ! printBoard - Clears terminal screen and then prints board
+    !
+    ! Inputs:
+    !   board - 2D array of logicals that represents state of game.
+
+        logical, dimension(:,:), intent(in) :: board
+        integer :: i, j
+
+        ! Clear Screen
+        call system('clear')
+
+        ! Print Board
+        do i = lbound(board,1), ubound(board,1)
+            do j = lbound(board,2), ubound(board,2)
+                if (board(i,j)) then
+                    write(*,"(A)", advance="no") '*' ! Life Cell is *
+                else
+                    write(*,"(A)", advance="no") ' ' ! Dead cell is blank
+                end if
+            end do
+            write(*,*) '' ! Line feed after current row
+        end do
+
+    end subroutine printBoard
+end module
+
 program life
 !
 ! Purpose:
@@ -7,10 +66,13 @@ program life
 !   Date        Programer       Description of change
 !   ====        =========       =====================
 !   2019-11-27  cchillen        Original Code
+use mBoard
+
 implicit none
 
 integer :: rows, cols           ! Board size
 logical, dimension (:, :), allocatable :: board ! Actual Board
+
 
 ! Basic Algorithm:
 ! 1. Prompt user to input board size
@@ -20,47 +82,12 @@ logical, dimension (:, :), allocatable :: board ! Actual Board
 ! 5. Loop at step 3 or quit
 
 ! Functions: createBoard(), stepBoard(), printBoard()
+rows = 10
+cols = 10
+
+board = createBoard(rows, cols)
+
+call printBoard(board)
 
 
 end program life
-
-function createBoard(rows, cols)
-    integer, intent(in) :: rows, cols
-    logical, dimension(:,:), allocatable :: board
-
-    createBoard = allocate( board(rows, cols) )
-
-end function createBoard
-
-function stepBoard(board)
-    logical, dimension(:,:), intent(in) :: board
-    logical, dimension(:,:), allocatable :: stepBoard
-
-end function stepBoard
-
-subroutine printBoard(board)
-! printBoard - Clears terminal screen and then prints board
-!
-! Inputs:
-!   board - 2D array of logicals that represents state of game.
-implicit none
-
-    logical, dimension(:,:), intent(in) :: board
-    integer :: i, j
-
-    ! Clear Screen
-    call system('clear')
-
-    ! Print Board
-    do i = lbound(board,1), ubound(board,1)
-        do j = lbound(board,2), ubound(board,2)
-            if (board(i,j)) then
-                write(*,*) '*' ! Life Cell is *
-            else
-                write(*,*) ' ' ! Dead cell is blank
-            end if
-        end do
-        write(*,*) '\n' ! Line feed after current row
-    end do
-
-end subroutine printBoard
